573aeeef231b20b77a8ad9252649d7d33e270c17
Ashley Wong <ashleysssmile@gmail.com>
2017-04-25 20:58:20 +0800

    New game

---
+++ app/src/main/java/com/example/ashleywong/myapplication/utility/Common.java
@@ -0,0 +1,28 @@
+package com.example.ashleywong.myapplication.utility;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+
+import com.example.ashleywong.myapplication.R;
+
+import java.io.IOException;
+
+public class Common {
+    public static Drawable loadImageFromAsset1(Context ctx, String image) {
+        try {
+            return Drawable.createFromStream(ctx.getAssets().open(image), null);
+        } catch (IOException e) {
+            return ctx.getResources().getDrawable(R.drawable.ic_launcher);
+        }
+    }
+
+    public static Bitmap loadImageFromAsset(Context ctx, String image) {
+        try {
+            return BitmapFactory.decodeStream(ctx.getAssets().open(image));
+        } catch (IOException e) {
+            return BitmapFactory.decodeResource(ctx.getResources(), R.drawable.ic_launcher);
+        }
+    }
+}
